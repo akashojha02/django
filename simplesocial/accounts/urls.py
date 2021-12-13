5b50a913b4f521fa76302330754a593012216a8b
akashojha02 <akashojha.u@gmail.com>
2021-12-13 22:32:01 +0530

    'search-funtion-added'

--- simplesocial/accounts/urls.py
+++ simplesocial/accounts/urls.py
@@ -7,7 +7,8 @@ app_name = 'accounts'
 urlpatterns=[
     path('login/',auth_views.LoginView.as_view(template_name='accounts/login.html'),name='login'),
     path('logout/',auth_views.LogoutView.as_view(),name='logout'),
-    path('signup/',views.SignUp.as_view(),name='signup')
+    path('signup/',views.SignUp.as_view(),name='signup'),
+    path('search/',views.search_profile,name='search-profile'),
 
 
 ]
--- simplesocial/accounts/views.py
+++ simplesocial/accounts/views.py
@@ -4,8 +4,31 @@ from django.urls import reverse_lazy
 from . import forms
 
 # Create your views here.
+from django.contrib.auth import get_user_model
+User= get_user_model()
+
 
 class SignUp(CreateView):
     form_class = forms.UserCreateForm
     success_url= reverse_lazy('login')
     template_name='accounts/signup.html'
+
+
+def search_profile(request):
+    if request.method=='POST':
+        searched= request.POST['searched']
+        user_list = User.objects.all()
+        # print(user_list)
+        searched_result = user_list.filter(username__icontains = searched)
+        # print(searched_result)
+        values = searched_result.values('username')
+        # print(values)
+        name=[]
+        for value in values:
+            print(value)
+            name.append(value['username'])
+        # print(name)
+        return render(request, 'accounts/search_people.html',{'searched':searched,
+                        'name':name})
+    else:
+        return render(request, 'accounts/search_people.html',{})
